dd5191e416d87186f7338ecbae8914e3c4fa751f
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-07-12 11:26:49 -0300

    adicionada a chave api em um documento de texto

--- my-yt/src/pages/Login.jsx
+++ my-yt/src/pages/Login.jsx
@@ -4,20 +4,20 @@ import { Link } from 'react-router-dom';
 export default class Login extends Component {
   constructor() {
     super()
-
+    
     this.state = {
       name: '',
       password: '',
     }
   }
-
+  
   handleChange = ({ target }) => {
     const { name, value } = target;
     this.setState({
       [name]: value,
     })
   }
-
+  
   render() {
     const { name, password } = this.state;
     return (
@@ -28,14 +28,14 @@ export default class Login extends Component {
           name="name"
           placeholder="Digite seu nome"
           value={ name }
-        />
+          />
         <input
           onChange={ this.handleChange }
           type="password"
           name="password"
           placeholder="Digite sua senha"
           value={ password }
-        />
+          />
         <Link to="/home">
           <button type="button">Entrar</button>
         </Link>
